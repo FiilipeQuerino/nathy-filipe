7bc481722f115aa7bec501c9b96e873241a58ee5
QUERINO <flpqrn@gmail.com>
2024-09-14 15:42:34 -0300

    teste de nova home

--- src/app/scripts/home.js
+++ src/app/scripts/home.js
@@ -34,9 +34,39 @@ document.addEventListener("DOMContentLoaded", function () {
             .catch(error => console.error('Erro ao carregar conteúdo:', error));
     }
 
+    // Delegação de evento para o container principal
+    document.getElementById('contentContainer').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Verifica se o alvo do evento é o formulário de confirmação
+        if (event.target.id === 'rsvp-form') {
+            // Código para enviar os dados do formulário
+            const formData = new FormData(event.target);
+            const data = Object.fromEntries(formData.entries());
+
+            // Envia os dados para o Google Sheets (ajuste a URL de acordo com o seu script)
+            fetch('https://script.google.com/macros/s/AKfycbz3wi1TacxhCiNDu37bq_uV0HkMpzXYp8uUorTz83OAAJkoRXQM5HvZtKyu62uftPQN/exec', {
+                method: 'POST',
+                mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(() => {
+                    // Mostra mensagem de sucesso
+                    alert('Confirmação enviada com sucesso!');
+                })
+                .catch(error => {
+                    console.error('Erro ao enviar os dados:', error);
+                    alert('Houve um erro ao enviar a confirmação. Por favor, tente novamente.');
+                });
+        }
+    });
+
     document.getElementById("confirmBtn").addEventListener('click', () => loadContent('confirm.html'));
     document.getElementById("locationBtn").addEventListener('click', () => loadContent('location.html'));
-    document.getElementById("giftBtn").addEventListener('click', () => { loadContent('giftgallery.html'); });    
+    document.getElementById("giftBtn").addEventListener('click', () => { loadContent('giftgallery.html'); });
     document.getElementById("dressCodeBtn").addEventListener('click', () => loadContent('dress-code.html'));
 
     // Inicializa com a seção de confirmação

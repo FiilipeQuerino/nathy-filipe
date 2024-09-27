1bf8bed633f39ea1092e220257e238f0f531dc55
Filipe Querino <72930009+FiilipeQuerino@users.noreply.github.com>
2024-09-27 09:52:43 -0300

    Delete src/app/scripts directory

--- src/app/scripts/dress-code.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Dress Code section loaded');
-    // Aqui você pode adicionar scripts adicionais, como animações ou eventos
-});

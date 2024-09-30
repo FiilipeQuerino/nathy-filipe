61f19732273bda58ac627f942578a831d01bec3c
Filipe Querino <72930009+FiilipeQuerino@users.noreply.github.com>
2024-09-30 19:08:10 -0300

    Update nf-animacao.js

--- src/app/scripts/nf-animacao.js
+++ src/app/scripts/nf-animacao.js
@@ -1,21 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    iniciarTransicao();
+    const animacaoContainer = document.querySelector('#nf-animacao');
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                document.querySelector('.letra-n').style.animationPlayState = 'running';
+                document.querySelector('.letra-f').style.animationPlayState = 'running';
+                document.querySelector('.coracao').style.animationPlayState = 'running';
+            }
+        });
+    }, { threshold: 0.5 });
+
+    observer.observe(animacaoContainer);
 });
-
-function iniciarTransicao() {
-    window.addEventListener('scroll', () => {
-        const letrasElement = document.getElementById('letras-secao');
-        const countdownSection = document.getElementById('countdown-section');
-
-        const letrasPosition = letrasElement.getBoundingClientRect().top;
-        const countdownPosition = countdownSection.getBoundingClientRect().top;
-
-        if (letrasPosition < window.innerHeight / 1.5) {
-            letrasElement.classList.add('juntar');
-        }
-
-        if (countdownPosition < window.innerHeight / 2) {
-            letrasElement.classList.add('mostrar-nomes');
-        }
-    });
-}
